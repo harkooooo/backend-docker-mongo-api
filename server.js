f690f53f639d31d250da0decc583395b2266ff06
haris <haris_25@hotmail.com>
2026-04-14 16:42:41 +0200

    Fix: dynamic port for deploy

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.get("/", (req, res) => {
 
 app.use("/users", userRoutes);
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
