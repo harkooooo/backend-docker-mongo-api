65706b6393756bb85a78adfae137a80dbc049884
haris <haris_25@hotmail.com>
2026-05-25 00:22:07 +0200

    Fix double password hashing and use deployed API URL

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,12 +11,10 @@ exports.register = async (req, res) => {
       return res.status(400).json({ error: "User already exists" });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-
     const user = await User.create({
       name,
       email,
-      password: hashedPassword,
+      password,
       age
     });
 
